e654042f2aefd34a7c9d0d00187756939ad0d816
garlicpollpoll <kyoungsuk3254@naver.com>
2021-12-29 01:22:22 +0900

    Description

--- src/main/java/hello/review/springcoreprinciple/order/OrderServiceImpl.java
+++ src/main/java/hello/review/springcoreprinciple/order/OrderServiceImpl.java
@@ -6,20 +6,16 @@ import hello.review.springcoreprinciple.discount.RateDiscountPolicy;
 import hello.review.springcoreprinciple.member.Member;
 import hello.review.springcoreprinciple.member.MemberRepository;
 import hello.review.springcoreprinciple.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
